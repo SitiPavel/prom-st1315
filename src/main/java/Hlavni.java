1d2710bc16287fb261480579e0396d255306c269
sitinpa1 <sitinpa1@uhk.cz>
2024-03-20 13:38:14 +0100

    rozšíření třídy hlavní a popsání metod v "trida"

--- src/main/java/Hlavni.java
+++ src/main/java/Hlavni.java
@@ -1,8 +1,8 @@
 public class Hlavni {
-    /**
-     * Hlavní třída projektu
-     */
     public static  void main(String[] args) {
+
         System.out.println("Hlavní třída");
+        System.out.println("přidáno v konu");
+
     }
 }
--- src/main/java/Trida.java
+++ src/main/java/Trida.java
@@ -1,4 +1,7 @@
 public class Trida {
+    /**
+     * vedlejší třída
+     */
     public static void main(String[] args) {
         System.out.println("Třída třída");
     }
